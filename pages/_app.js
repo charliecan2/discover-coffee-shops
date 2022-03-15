0d67cbb96ba9757925c456e6f2032cf6e410b542
Carlos Cantu <carloscq920@gmail.com>
2022-03-14 23:40:01 -0500

    Created the beginnings of the homepage

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
+import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <Component {...pageProps} />
+    <footer className={styles.footer}>
+      <p>Made with ❤️ by Carlos Cantu</p>
+    </footer>
+  </>
+  )
 }
 
 export default MyApp
