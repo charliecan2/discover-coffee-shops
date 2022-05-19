744efff7423a1930f41c59d18f5f0e275d3c79cd
Carlos Cantu <carloscq920@gmail.com>
2022-05-19 15:03:16 -0500

    createCoffeeStore api can now create a record using the create method from AirtableAPI, and it is returned to us as a json response

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -11,7 +11,7 @@ export default async function createCoffeeStore(req, res){
   if(req.method = "POST"){
     try {
       const findCoffeeStoreRecords = await table.select({
-        filterByFormula: `id="0"`
+        filterByFormula: `id="1"`
       }).firstPage()
 
       console.log({findCoffeeStoreRecords})
@@ -26,7 +26,28 @@ export default async function createCoffeeStore(req, res){
         res.json(records);
       } else {
         // create record
-        res.json({ message: "Create record"});
+        const createRecords = await table.create(
+          [
+            {
+              "fields": {
+                id: "1",
+                name: "My Favorite Coffee Store",
+                address: "101 North Street",
+                neighborhood: "Some Neighborhood",
+                voting: 200,
+                imgUrl: "https://images.unsplash.com/photo-1453614512568-c4024d13c247?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8Y29mZmVlJTIwc2hvcHxlbnwwfHwwfHw%3D&w=1000&q=80"
+              }
+            }
+          ]
+        )
+
+        const records = createRecords.map((record) => {
+          return {
+            ...record.fields,
+          }
+        })
+
+        res.json({ records });
       }
     } catch(error) {
       console.error("Error finding store", error);
